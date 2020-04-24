cd50eefc04039235eb18d585d1e83881e445d1de
Nayeem Hossain <nayeem9812@gmail.com>
2020-04-24 20:33:13 +0600

    Update ssl.sh

--- ssl.sh
+++ ssl.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-echo "Enter your domain names (domain.com,www.domain.com)..."
-read domains
+./update.sh
 
-echo "Enter your project directory path (/var/www/domain/public)..."
-read path
-
-echo "Installing SSL for ${domains}..."
-sudo certbot certonly --webroot --webroot-path=${path} -d ${domains}
+sudo certbot --apache
