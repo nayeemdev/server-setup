6604f0ee612bd406f01fafe3b71a0fb17ca0be80
Md Nayeem Hossain <nayeem9812@gmail.com>
2021-01-27 12:31:13 +0600

    fix: Spelling mistake

--- apache2.sh
+++ apache2.sh
@@ -6,4 +6,4 @@ echo "Installing Apache2 Server"
 sudo apt-get install apache2
 
 echo "********** Apache2 Server Setup Done **********"
-echo "To verify your apache goto you ip with browser"
+echo "To verify your apache server goto your IP with browser."
