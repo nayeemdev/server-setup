0f6d2285a58bc21fa4a5bc9410338725f1f7d5cc
Md Nayeem Hossain <nayeem9812@gmail.com>
2021-03-23 12:32:42 +0600

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ "$(whoami)" != 'root' ]; then
+	echo -e $"\nYou dont have permission to run this script please login as root with sudo -s or use sudo.\n"
+		exit 1;
+fi
+
 echo "Updating your required package and dependencies..."
 sudo apt update -y
 sudo apt dist-upgrade -y
